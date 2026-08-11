f4c60adcb2f1b2b82af8a29b7d1d3dac0b39e1bf
nebasishp <aryandsalgo@gmail.com>
2026-08-11 19:01:16 +0530

    added canary version

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello EKS Pipeline";
+    public String home() {
+        return "CANARY VERSION";
     }
 
     @GetMapping("/hello2")
